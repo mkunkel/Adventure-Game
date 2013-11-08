a892d4d6b68d9f59ecca72e8424557f1f998e100
Peter Himmelreich <averygoodplanet@gmail.com>
2013-11-08 15:39:31 -0600

    Passing and receiving data from game.create (next: save game and return game object)

--- routes/game.js
+++ routes/game.js
@@ -13,6 +13,15 @@ exports.index = function(req, res){
 // POST /
 exports.create = function(req, res){
   console.log('game.create'.italic.underline.bold.green);
+  console.log('req.body.playerName');
+  console.log(req.body.playerName);
+  console.log('req.body.columns');
+  console.log(req.body.columns);
+  console.log('req.body.rows');
+  console.log(req.body.rows);
+  res.send({ playerName: req.body.playerName});
+  //Need to create and save a new game in database
+  //in callback pass the game object back to the browser
 };
 
 // PUT /
