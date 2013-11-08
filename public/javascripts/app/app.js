784f77933859b1b1ffc0dbc8093840c340b991b8
Rachel Werner <rachelannwerner@gmail.com>
2013-11-08 15:57:17 -0600

    table generation

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -10,3 +10,18 @@ function keyHandler(e) {
   var key = String.fromCharCode(keyCode);
   console.log(key);
 }
+var game = {};
+game.columns = 5;//game.columns;
+game.rows = 6;//game.rows;
+//#gameBoard
+
+//function buildGameBoard(game.columns, game.rows){
+for(var i = 0; i < game.rows; i++){
+  var $tr = $('<tr></tr>').attr('data-y', i);
+  for(var a = 0; a < game.columns; a++){
+    var $td = $('<td></td>').attr('data-x', a);
+    $($tr).append($td);
+  };
+  $('#gameBoard').append($tr);
+};
+
