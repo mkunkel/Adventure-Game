2e212506e0a1a4b29328767e1a3f2d8f7b104e72
Mike Kunkel <yourmom.tv@gmail.com>
2013-11-09 13:04:48 -0600

    rotating wormhole

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -90,12 +90,16 @@ function htmlUpdatePieces(){
   for(var i = 0; i < game.movingPieces.length; i++){
     var $square = $('tr[data-y="' + game.movingPieces[i].position.y + '"] td[data-x="' + game.movingPieces[i].position.x + '"]');
     var $piece = $('<img class="piece" src="../images/'+ game.movingPieces[i].type +'.png"/>');
+    $piece.addClass(game.movingPieces[i].type);
     $square.append($piece);
   };
   //affix stationary pieces to board:
   for(var i = 0; i < game.stationaryPieces.length; i++){
     var $square = $('tr[data-y="' + game.stationaryPieces[i].position.y + '"] td[data-x="' + game.stationaryPieces[i].position.x + '"]');
     var $piece = $('<img src="../images/' + game.stationaryPieces[i].type +'.png"/>').addClass('piece');
+    if (game.stationaryPieces[i].type != 'princess' && game.stationaryPieces[i].type != 'treasure'){
+      $piece.addClass(game.stationaryPieces[i].type);
+    }
     $square.append($piece);
   };
 
